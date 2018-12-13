0808e962ae05730e1e44826353a316cfd550cdbb
c6burns <chris@fursman.com>
2018-12-13 13:06:45 -0700

    original checkin

---
+++ Assets/SurfaceBuffers/SurfaceBuffers.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+[RequireComponent(typeof(Camera))]
+public class SurfaceBuffers : MonoBehaviour
+{
+	[SerializeField] Material material;
+	[SerializeField] ComputeShader cs;
+	ComputeBuffer vertexBuffer;
+	ComputeBuffer vertexBufferOrig;
+	const int triangleCount = 1024;
+	const int vertexCount = triangleCount * 3;
+	const float range = 5.0f;
+	const float twidth = 0.1f;
+	Material _material;
+	float elapsedTime = 0f;
+
+	struct CustomVertex {
+		public Vector3 position;
+		public Color color;
+	};
+
+	void Start()
+	{
+		_material = new Material(material);
+		_material.name += " (clone)";
+
+		CustomVertex[] points = new CustomVertex[vertexCount];
+		vertexBuffer = new ComputeBuffer(vertexCount, Marshal.SizeOf(typeof(CustomVertex)), ComputeBufferType.Default);
+		vertexBufferOrig = new ComputeBuffer(vertexCount, Marshal.SizeOf(typeof(CustomVertex)), ComputeBufferType.Default);
+		
+		Random.InitState(0);
+
+		Vector3 p0off = Vector3.zero, p1off = Vector3.zero, p2off = Vector3.zero;
+		p0off.x = twidth;
+		p1off.y = twidth;
+		p2off.x = -twidth;
+		Vector3 position = Vector3.zero;
+		Color startColor = Color.black;
+		int v;
+		for (int t = 0; t < triangleCount; t++) {
+			position.x = Random.Range(-range, range);
+			position.y = Random.Range(-range, range);
+
+			v = t * 3;
+
+			points[v + 0].position = position + p0off;
+			points[v + 0].color = startColor;
+			points[v + 1].position = position + p1off;
+			points[v + 1].color = startColor;
+			points[v + 2].position = position + p2off;
+			points[v + 2].color = startColor;
+		}
+		vertexBuffer.SetData(points);
+		vertexBufferOrig.SetData(points);
+	}
+
+	void OnDestroy()
+	{
+		vertexBuffer.Release();
+		vertexBufferOrig.Release();
+		Destroy(_material);
+	}
+
+	void Update()
+	{
+		elapsedTime += Time.deltaTime;
+
+		// just update all uniforms here and use string IDs ... performance doesn't matter
+		uint tx, ty, tz;
+		int csKID = cs.FindKernel("MovePoints");
+		cs.GetKernelThreadGroupSizes(csKID, out tx, out ty, out tz);
+		cs.SetBuffer(csKID, "vertexBufferRead", vertexBufferOrig);
+		cs.SetBuffer(csKID, "vertexBufferWrite", vertexBuffer);
+		cs.SetInt("vertexCount", vertexCount);
+		cs.SetFloat("elapsedTime", elapsedTime);
+		cs.Dispatch(csKID, vertexCount / (int)tx + 1, 1, 1);
+	}
+
+	void OnPostRender()
+	{
+		// just update all uniforms here and use string IDs ... performance doesn't matter
+		_material.SetBuffer("vertexBuffer", vertexBuffer);
+		_material.SetMatrix("_LocalToWorld", Matrix4x4.identity);
+		_material.SetMatrix("_WorldToLocal", Matrix4x4.identity);
+		_material.SetPass(0);
+		Graphics.DrawProcedural(MeshTopology.Triangles, vertexCount, 1);
+	}
+}
---
+++ Assets/SurfaceBuffers/SurfaceBuffers.shader
@@ -0,0 +1,84 @@
+﻿Shader "Custom/SurfaceBuffers" {
+	Properties {
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+
+		Cull Off
+
+		CGPROGRAM
+
+		#pragma surface surf Standard vertex:vert addshadow
+		#pragma instancing_options procedural:setup
+		#pragma target 3.5
+
+		#if SHADER_TARGET >= 35 && (defined(SHADER_API_D3D11) || defined(SHADER_API_GLES3) || defined(SHADER_API_GLCORE) || defined(SHADER_API_XBOXONE) || defined(SHADER_API_PSSL) || defined(SHADER_API_SWITCH) || defined(SHADER_API_VULKAN) || (defined(SHADER_API_METAL) && defined(UNITY_COMPILER_HLSLCC)))
+			#define SUPPORT_STRUCTUREDBUFFER
+		#endif
+
+		#if (defined(UNITY_INSTANCING_ENABLED) || defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)) && defined(SUPPORT_STRUCTUREDBUFFER)
+			#define ENABLE_INSTANCING
+		#endif
+
+		half _Glossiness;
+		half _Metallic;
+		float4x4 _LocalToWorld;
+		float4x4 _WorldToLocal;
+
+#ifdef ENABLE_INSTANCING
+		struct CustomVertex {
+			float3 position;
+			float4 color;
+		};
+
+		StructuredBuffer<CustomVertex> vertexBuffer;
+#endif
+
+		struct Input {
+			float4 color;
+		};
+
+		struct appdata {
+			float4 vertex : POSITION;
+			float3 normal : NORMAL;
+			float4 tangent : TANGENT;
+			float4 texcoord1 : TEXCOORD1;
+			float4 texcoord2 : TEXCOORD2;
+			uint vid : SV_VertexID;
+			UNITY_VERTEX_INPUT_INSTANCE_ID
+		};
+
+		void setup()
+		{
+			unity_ObjectToWorld = _LocalToWorld;
+			unity_WorldToObject = _WorldToLocal;
+		}
+		
+		void vert(inout appdata v, out Input o)
+		{
+			UNITY_INITIALIZE_OUTPUT(Input, o);
+
+#ifdef ENABLE_INSTANCING
+			v.vertex = float4(vertexBuffer[v.vid].position, 1);
+			o.color = vertexBuffer[v.vid].color;
+#else
+			v.vertex = float4(0, 0, 0, 1);
+			o.color = float4(1, 0, 0, 1);
+#endif
+
+			v.normal = normalize(ObjSpaceViewDir(v.vertex));
+		}
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			float4 c = IN.color;
+			o.Albedo = c.rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
